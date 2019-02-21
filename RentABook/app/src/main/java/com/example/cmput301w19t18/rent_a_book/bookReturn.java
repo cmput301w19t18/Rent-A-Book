f4e3238daed441d6c055f3f554a3dcd5eec53583
Oanderso <oanderso@ualberta.ca>
2019-02-21 10:56:45 -0700

    Create bookReturn.java

---
+++ RentABook/app/src/main/java/com/example/cmput301w19t18/rent_a_book/bookReturn.java
@@ -0,0 +1,22 @@
+package com.example.cmput301w19t18.rent_a_book;
+
+public class bookReturn {
+
+    private Boolean returnConfirmed;
+    private Book book;
+
+    public bookReturn () {
+        //code
+    }
+
+    private void checkReturn (String borrowerEmail, String ownerEmail) {
+        //make both users confirm the return
+    }
+    
+    private void statusToAvailable (Boolean returnConfirmed) {
+        //sets the book's status to available
+    }
+
+
+
+}
