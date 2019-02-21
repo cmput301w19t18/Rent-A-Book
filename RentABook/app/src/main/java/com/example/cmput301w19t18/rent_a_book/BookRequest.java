41ac14746c87f958e1ef78a9c427b9d4ff6bb3ef
Oanderso <oanderso@ualberta.ca>
2019-02-21 10:56:43 -0700

    Create BookRequest.java

---
+++ RentABook/app/src/main/java/com/example/cmput301w19t18/rent_a_book/BookRequest.java
@@ -0,0 +1,25 @@
+package com.example.cmput301w19t18.rent_a_book;
+
+public class BookRequest {
+
+    private Boolean availableStatus;
+    private String requestMessage;
+    private Book book;
+
+    public BookRequest(){
+        //code
+    }
+
+    private void checkAvailableStatus (){
+        //code
+    }
+
+    private void sendRequestMessage () {
+        //code
+    }
+
+    private void statusToRequested () {
+        //code
+    }
+
+}
