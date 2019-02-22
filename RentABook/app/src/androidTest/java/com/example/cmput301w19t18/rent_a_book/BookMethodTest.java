0167e8f115736e9ce37ad02abc57a7e426c04048
rsander0896 <46581959+rsander0896@users.noreply.github.com>
2019-02-21 20:55:57 -0700

    Book Tests 1
    Began initial unit tests for book class

---
+++ RentABook/app/src/androidTest/java/com/example/cmput301w19t18/rent_a_book/BookMethodTest.java
@@ -0,0 +1,19 @@
+package com.example.cmput301w19t18.rent_a_book;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class BookMethodTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+}
