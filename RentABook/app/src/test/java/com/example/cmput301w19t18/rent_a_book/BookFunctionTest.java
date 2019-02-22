332ddf338b74ddd5c238b4989b8efdd82d5cccbb
rsander0896 <46581959+rsander0896@users.noreply.github.com>
2019-02-21 21:19:10 -0700

    Tests Implemented
    Implemented the test for setBorrowedBy function.

---
+++ RentABook/app/src/test/java/com/example/cmput301w19t18/rent_a_book/BookFunctionTest.java
@@ -0,0 +1,29 @@
+package com.example.cmput301w19t18.rent_a_book;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BookFunctionTest {
+
+    @Test
+    public void setRequestedBy() {
+        Book obj = new Book("Title", "Author", "Genre", "1234567890", "Available", "Owner", 5);
+        obj.setRequestedBy("Borrower");
+
+    }
+
+    @Test
+    public void setBorrowedBy() {
+        Book obj = new Book( "Title", "Author", "Genre", "1234567890", "Available", "Owner", 5);
+        obj.setBorrowedBy("Borrower");
+        String status = obj.getBstatus();
+        String borrower = obj.getBorrowedBy();
+
+        assertEquals(status, "Borrowed");
+        assertEquals(borrower, "Borrower");
+    }
+
+}
