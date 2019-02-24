df2738da4466a26af52072342ad7a97391c4792a
JuSong Baek <jusong@cs.ualberta.ca>
2019-02-23 19:13:01 -0700

    added GeoLocation and SearchResultsActivity classes

---
+++ RentABook/app/src/main/java/com/example/cmput301w19t18/rent_a_book/SearchResultsActivity.java
@@ -0,0 +1,15 @@
+package com.example.cmput301w19t18.rent_a_book;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SearchResultsActivity extends AppCompatActivity {
+
+    // I don't know if I should implement the search results as a separate class in addition to this activity lol
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_results);
+    }
+}
