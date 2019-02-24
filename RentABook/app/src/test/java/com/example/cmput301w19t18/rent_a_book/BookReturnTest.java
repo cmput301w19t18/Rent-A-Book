16b1ab76cfccadf85cf3519ba81800058290bbbd
Oanderso <oanderso@ualberta.ca>
2019-02-24 11:44:46 -0700

    Create BookReturnTest.java

---
+++ RentABook/app/src/test/java/com/example/cmput301w19t18/rent_a_book/BookReturnTest.java
@@ -0,0 +1,22 @@
+package com.example.cmput301w19t18.rent_a_book;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BookReturnTest {
+
+    Book obj = new Book("Title", "Author", "Genre", "1234567890", "Borrowed", "Owner", 5);
+
+    String borrowerEmail = "borrower@gmail.com";
+    String ownerEmail = "owner@gmail.com";
+
+    @Test
+    public void checkReturn ( String borrowerEmail, String ownerEmail){
+        assertEquals("borrower@gmail.com", borrowerEmail );
+        assertEquals("owner@gmail.com", ownerEmail );
+    }
+
+
+
+}
