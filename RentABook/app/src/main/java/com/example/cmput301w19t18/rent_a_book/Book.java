9dd607787dc6a9220595d851e647a2b0dde86416
Oanderso <oanderso@ualberta.ca>
2019-03-08 15:59:05 -0700

    Update Book.java

--- RentABook/app/src/main/java/com/example/cmput301w19t18/rent_a_book/Book.java
+++ RentABook/app/src/main/java/com/example/cmput301w19t18/rent_a_book/Book.java
@@ -1,19 +1,74 @@
 package com.example.cmput301w19t18.rent_a_book;
 
 import java.util.ArrayList;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class Book {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class Book extends AppCompatActivity{
 
     private String btitle;
     private String author;
-    private String[] genre;
     private String ISBN; //Established as an ISBN to allow for better error handling and to prevent dropping of leading 0's
     private String bstatus;
     private String owner;
     private Integer rating;
-
-    private ArrayList<String> requestedBy; //Emails of users that have placed requests for the book
     private String borrowedBy; //Email of the user that borrowed the book
+    private Integer copyCount; //number of copies of the book that exist
+
+    private String[] genre;
+    private ArrayList<String> requested_by;
+    private ArrayList<String> owned_by;
+    private ArrayList<String> requestedBy;
+    //private ArrayList<String> requestedBy; //Emails of users that have placed requests for the book
+
+    //firebase auth object
+    private FirebaseAuth bAuth;
+    private DatabaseReference databaseReference;
+
+    //buttons and fields
+    private EditText editTextName, editTextAddress;
+    private Button SubmitB;
+    private EditText ISBNF;
+    private EditText AuthorF;
+    private EditText TitleF;
+    private EditText DescF;
+
+    @Override
+    //now createes a view
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_newbook);
+
+        //initializing firebase auth object
+        bAuth = FirebaseAuth.getInstance();
+
+        //initializing fields and buttons
+        SubmitB = (Button) findViewById(R.id.SubmitButton);
+
+        ISBNF = (EditText) findViewById(R.id.ISBNBox);
+        AuthorF = (EditText) findViewById(R.id.AuthBox);
+        TitleF = (EditText) findViewById(R.id.TitleBox);
+        DescF = (EditText) findViewById(R.id.DescriptionBox);
+
+        //check if user is logged in. if not, returns null
+        if (bAuth.getCurrentUser() == null){
+            finish(); //close activity
+            startActivity(new Intent(this, MainActivity.class)); //returns to login screen
+        }
+
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+
+    }
 
     //constructor (changed to public constructor)
     public Book (String btitle, String author, String[] genre, String ISBN, String bstatus, String owner, Integer rating){
@@ -121,4 +176,52 @@ public class Book {
         this.requestedBy = initial;
         return;
     }
+
+
+    ////////////// Book operations //////////////
+
+
+    private void InputNewBook(){
+        String author = AuthorF.getText().toString().trim();
+        String ISBN = ISBNF.getText().toString().trim();
+        String title = TitleF.getText().toString().trim();
+        String description = DescF.getText().toString().trim();
+        String[] genre = {"test genre"};
+        String owner = "test owner";
+        Integer rating = 4;
+        String status = "available";
+
+        Book book = new Book(title, author, genre, ISBN, status, owner, rating );
+
+        databaseReference.child("books").setValue(book); //should put the book in the db under books
+
+        Toast.makeText(this, "Uploaded.", Toast.LENGTH_LONG).show();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
