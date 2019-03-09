d0bcc27353a276624af22c7b5aa99f50c624540d
jdikova <dikova@ualberta.ca>
2019-03-08 21:15:23 -0700

    genre swiping activity works, currently making it less ugly

---
+++ RentABook/app/src/main/java/com/example/cmput301w19t18/rent_a_book/GenreTab1.java
@@ -0,0 +1,17 @@
+package com.example.cmput301w19t18.rent_a_book;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class GenreTab1 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_pick_genre_pref, container, false);
+
+    }
+}
--- RentABook/app/src/main/java/com/example/cmput301w19t18/rent_a_book/PickGenrePreference.java
+++ RentABook/app/src/main/java/com/example/cmput301w19t18/rent_a_book/PickGenrePreference.java
@@ -1,39 +1,57 @@
 package com.example.cmput301w19t18.rent_a_book;
 
-import android.content.Intent;
+import android.app.ActionBar;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
 
-import java.util.ArrayList;
-
-public class PickGenrePreference extends FragmentActivity {
+public class PickGenrePreference extends AppCompatActivity {
     static final int numPages = 3;
     private int[] preferenceList = new int [18];
     private int selectedNum = 0;
-    private Button romance;
+    private int tabIcon = R.drawable.circle_unselected_gray_5dp;
 
-    BookFragmentAdapter bfAdapter;
+   private int[] tabIcons = {
+            R.drawable.circle_unselected_gray_5dp,
+            R.drawable.circle_unselected_gray_5dp,
+            R.drawable.circle_unselected_gray_5dp,
+    };
 
-    ViewPager bvPager;
+    private GenreAdapter genreAdapter;
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pick_genre_pref);
 
-        // bfAdapter = new BookFragmentAdapter(getSupportFragmentManager());
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        //  bvPager = (ViewPager) findViewById(R.id.pager);
-        //  bvPager.setAdapter(bfAdapter);
 
-        romance = (Button) findViewById(R.id.romanceButton);
+        setContentView(R.layout.custom_genre_pick_tab);
 
+        viewPager = (ViewPager) findViewById(R.id.custPager);
+        tabLayout = (TabLayout) findViewById(R.id.custTabLayout);
+
+        genreAdapter = new GenreAdapter(getSupportFragmentManager());
+        genreAdapter.addFragment(new GenreTab1(), "Page 1");
+        genreAdapter.addFragment(new GenreTab2(), "Page 2");
+        genreAdapter.addFragment(new GenreTab3(), "Page 3");
+
+        viewPager.setAdapter(genreAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+        // setting the first screen
+        //viewPager.setCurrentItem(0);
+
+        //romance = (Button) findViewById(R.id.button2);
+        //romance.setText("Romance");
+/*
         romance.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -53,5 +71,7 @@ public class PickGenrePreference extends FragmentActivity {
                 }
             }
         });
+*/
     }
+
 }
