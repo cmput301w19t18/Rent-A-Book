bb87e51e16732bcc6a5586bfdc5355f1b5755991
jdikova <dikova@ualberta.ca>
2019-03-10 17:54:05 -0600

    updated

--- RentABook/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/design/R.java
+++ RentABook/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/design/R.java
@@ -995,8 +995,8 @@ public final class R {
         public static final int notification_template_icon_low_bg = 0x7f07008f;
         public static final int notification_tile_bg = 0x7f070090;
         public static final int notify_panel_notification_icon_bg = 0x7f070091;
-        public static final int tooltip_frame_dark = 0x7f070092;
-        public static final int tooltip_frame_light = 0x7f070093;
+        public static final int tooltip_frame_dark = 0x7f070093;
+        public static final int tooltip_frame_light = 0x7f070094;
     }
     public static final class id {
         private id() {}
--- RentABook/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/example/cmput301w19t18/rent_a_book/R.java
+++ RentABook/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/example/cmput301w19t18/rent_a_book/R.java
@@ -4785,9 +4785,11 @@ public final class R {
     public static final int notification_template_icon_low_bg=0x7f07008f;
     public static final int notification_tile_bg=0x7f070090;
     public static final int notify_panel_notification_icon_bg=0x7f070091;
-    public static final int tooltip_frame_dark=0x7f070092;
-    public static final int tooltip_frame_light=0x7f070093;
-    public static final int unselected_circle_light_gray_10dp=0x7f070094;
+    public static final int selected_circle_7dp=0x7f070092;
+    public static final int tooltip_frame_dark=0x7f070093;
+    public static final int tooltip_frame_light=0x7f070094;
+    public static final int unselected_circle_7dp=0x7f070095;
+    public static final int unselected_circle_light_gray_10dp=0x7f070096;
   }
   public static final class id {
     public static final int ALT=0x7f080000;
