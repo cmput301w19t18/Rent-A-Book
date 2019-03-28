e3f760a5067f47e49de1b4e0ef6b7a060aaa2d64
Oanderso <oanderso@ualberta.ca>
2019-03-27 20:26:04 -0600

    Added map tests

---
+++ RentABook/app/src/test/java/com/example/cmput301w19t18/rent_a_book/MapTest.java
@@ -0,0 +1,26 @@
+package com.example.cmput301w19t18.rent_a_book;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MapTest {
+
+    MapsActivity MapTestObj = new MapsActivity();
+
+    @Test
+    public void checkAvailableStatus (){
+
+        MapTestObj.setLocationLat(150.0);
+        MapTestObj.setLocationLon(-50.0);
+
+        Double lat = MapTestObj.getLocationLat();
+        Double lon = MapTestObj.getLocationLon();
+
+        Double latlat = 150.0;
+        Double lonlon = -50.0;
+        
+        assertEquals(lat, latlat);
+        assertEquals(lon, lonlon);
+    }
+}
