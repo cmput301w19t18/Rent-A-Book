5fca8033b38fc57c9f72edce02440ca8c50bca54
Oanderso <oanderso@ualberta.ca>
2019-03-29 16:21:08 -0600

    Added test classes

---
+++ RentABook/app/src/test/java/com/example/cmput301w19t18/rent_a_book/BookDetailsTest.java
@@ -0,0 +1,16 @@
+package com.example.cmput301w19t18.rent_a_book;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BookDetailsTest {
+
+    @Test
+    public void homeDetails() {
+    }
+
+    @Test
+    public void setBookTitle() {
+    }
+}
