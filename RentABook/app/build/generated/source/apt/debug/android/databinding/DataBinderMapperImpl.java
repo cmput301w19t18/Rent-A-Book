313816a8e992dde26180c03b447c07440a475c47
jdikova <dikova@ualberta.ca>
2019-03-30 12:30:05 -0600

    removed debug files

--- RentABook/app/build/generated/source/apt/debug/android/databinding/DataBinderMapperImpl.java
+++
@@ -1,7 +0,0 @@
-package android.databinding;
-
-public class DataBinderMapperImpl extends MergedDataBinderMapper {
-  DataBinderMapperImpl() {
-    addMapper(new com.example.cmput301w19t18.rent_a_book.DataBinderMapperImpl());
-  }
-}
