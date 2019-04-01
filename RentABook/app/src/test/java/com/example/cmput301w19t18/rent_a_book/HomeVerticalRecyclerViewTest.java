dc3ffb655bc43bf591da3173a87726b8f73a311b
Oanderso <46537127+Oanderso@users.noreply.github.com>
2019-04-01 13:26:07 -0600

    Update HomeVerticalRecyclerViewTest.java

--- RentABook/app/src/test/java/com/example/cmput301w19t18/rent_a_book/HomeVerticalRecyclerViewTest.java
+++ RentABook/app/src/test/java/com/example/cmput301w19t18/rent_a_book/HomeVerticalRecyclerViewTest.java
@@ -2,11 +2,25 @@ package com.example.cmput301w19t18.rent_a_book;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 
 public class HomeVerticalRecyclerViewTest {
 
+    ArrayList<Category> arrayList = new ArrayList<Category>();
+    Category testCat1 = new Category();
+    Category testCat2 = new Category();
+
+
     @Test
-    public void getItemCount() {
+    public void getItemCountTest() {
+        arrayList.add(testCat1);
+        arrayList.add(testCat2);
+
+        String mevar = "me";
+        assertEquals(arrayList.size(), 2);
+
     }
 }
