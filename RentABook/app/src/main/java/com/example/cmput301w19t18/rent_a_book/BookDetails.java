74d1490a169ad27a9d69730c6ef6bf73693bc6c0
jdikova <dikova@ualberta.ca>
2019-04-03 01:51:28 -0600

    homeactivity crashing fixed; nav bar added to bookdetails

--- RentABook/app/src/main/java/com/example/cmput301w19t18/rent_a_book/BookDetails.java
+++ RentABook/app/src/main/java/com/example/cmput301w19t18/rent_a_book/BookDetails.java
@@ -1,9 +1,12 @@
 package com.example.cmput301w19t18.rent_a_book;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -61,8 +64,49 @@ public class BookDetails extends AppCompatActivity  implements View.OnClickListe
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        String mode = "1";
         setContentView(R.layout.activity_book_details2);
+
+        BottomNavigationView bnv = (BottomNavigationView) findViewById(R.id.navView);
+
+
+        bnv.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                        Intent intent;
+                        switch (menuItem.getItemId()) {
+                            case R.id.home:
+                                // do nothing because we're already here
+                                //Toast.makeText(getApplicationContext(), "home", Toast.LENGTH_LONG).show();
+                                Intent intent1;
+                                intent1 = new Intent(BookDetails.this, HomeActivity.class);
+                                startActivity(intent1);
+                                break;
+                            case R.id.search:
+                                //Toast.makeText(getApplicationContext(), "search", Toast.LENGTH_LONG).show();
+                                Intent intent2;
+                                intent2 = new Intent(BookDetails.this, SearchResultsActivity.class);
+                                startActivity(intent2);
+                                break;
+                            case R.id.inbox:
+                                //Toast.makeText(getApplicationContext(), "inbox", Toast.LENGTH_LONG).show();
+                                Intent intent3 = new Intent(BookDetails.this, Inbox.class);
+                                startActivity(intent3);
+                                break;
+                            case R.id.profile:
+                                //Toast.makeText(getApplicationContext(), "profile", Toast.LENGTH_LONG).show();
+                                Intent intent4;
+                                intent4 = new Intent(BookDetails.this, ProfileActivity.class);
+                                startActivity(intent4);
+                                break;
+                        }
+                        return false;
+                    }
+                });
+
+
+        String mode = "1";
         BookList = new ArrayList<>();
         homeBookList = new ArrayList<>();
         mDatabase = FirebaseDatabase.getInstance().getReference("Books");
